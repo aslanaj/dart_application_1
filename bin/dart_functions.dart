07ceec4ca93f151fcbe7e5598aa6059d44e7bd4a
aslanaj <theajaslan@gmai.com>
2023-05-28 05:54:24 +0600

    second commit

---
+++ bin/dart_functions.dart
@@ -0,0 +1,20 @@
+void main(){
+sum();
+
+var res = div(5, 7);
+
+print('Res: $res');
+}
+
+
+void sum(){
+  var a = 5, b = 15;
+  print('${a+b}');
+}
+
+int div(a,b){
+  return a + b;
+}
+
+
+int mines(a, b) => a + b;
